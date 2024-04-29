2444f2d8fe4d9052fdf35df8ab87f208ba437eae
Lam Le <lam-lh@genkisystem.com>
2024-04-29 23:47:21 +0700

    Fix web

--- lib/services/firebase/firebase_service.dart
+++ lib/services/firebase/firebase_service.dart
@@ -2,6 +2,13 @@ import 'package:firebase_core/firebase_core.dart';
 
 class FirebaseService {
   static Future<void> initFirebaseService() async {
-    await Firebase.initializeApp();
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+      apiKey: 'AIzaSyA3aALIDMsoAlJlAtrUGtf0Q4uQBdZ0LGc',
+      appId: '1:149066493429:web:c62ef8586a7662042904bc',
+      messagingSenderId: '149066493429',
+      projectId: 'pocketbook-75861',
+      storageBucket: 'pocketbook-75861.appspot.com',
+    ));
   }
 }
